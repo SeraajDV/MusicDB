d3b27a0fb26fb6cec25e264bbdb380779a55813b
SeraajDV <SeraajDV@users.noreply.github.com>
2019-06-25 12:42:43 +0200

    Using Column labels instead

--- src/com/seraajdv/model/Datasource.java
+++ src/com/seraajdv/model/Datasource.java
@@ -207,13 +207,13 @@ public class Datasource {
 
     public int getCount (String table) {
 
-        String sql = "SELECT COUNT(*), MIN(_id)  FROM " + table;
+        String sql = "SELECT COUNT(*) AS count, MIN(_id) AS min_id  FROM " + table;
 
         try (Statement statement = conn.createStatement();
                ResultSet results = statement.executeQuery(sql)) {
 
-            int count = results.getInt(1);
-            int min = results.getInt(2);
+            int count = results.getInt("count");
+            int min = results.getInt("min_id");
 
             System.out.format("Count = %d, Min = %d\n", count, min);
             return count;
